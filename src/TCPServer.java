4754716738cc38419b777a5d945d017b4e7f251c
Binary&&Entropy <kevinhu253216@gmail.com>
2018-07-07 16:51:13 -0400

    Update TCPServer.java

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -3,9 +3,10 @@ import java.net.*;
 public class TCPServer {
     public static void main(String[] args) throws Exception{
         ServerSocket ss = new ServerSocket(6666);
-        ss.accept();
-        System.out.println("A client is accept!");
-
+        while(true){
+            Socket s =ss.accept();
+            System.out.println("A client is accept!");
+        }
 
     }
 
